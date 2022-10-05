bab2107f4934f7c23cc6362deccf7022f13790a8
nicbritz89 <111434923+nicbritz89@users.noreply.github.com>
2022-10-05 16:36:26 +0200

    Add files via upload

---
+++ W3D1HW.js
@@ -0,0 +1,14 @@
+let myFirstName = "Nic";
+let mySurName= "Britz";
+let myMiddleName= "Frank";
+
+console.log (
+"Hello, my name is " + " " + myFirstName + " " +  myMiddleName + " " + mySurName
+)
+let myAge = 33;
+let currentYear= 2022;
+
+let myBirthYear = currentYear - myAge;
+
+console.log("My Birthyear is " + " " + myBirthYear);
+
